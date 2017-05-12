b15804609682c6a61283d4f9047fe1a541627fc3
qyf1994 <qyf1994@gmail.com>
2017-05-12 11:45:33 +0800

    fix chat

--- public/js/common.js
+++ public/js/common.js
@@ -10,11 +10,11 @@ login = function () {
         ]
     });
 }
-bgColor = function(val,col,nRow){
-    if(parseFloat(val) >0 ){
-        $("td:eq("+col +")",nRow).text("+"+ val).addClass('error');
-    }else if(parseFloat(val) <= 0){
-         $("td:eq("+col +")",nRow).addClass('ssuccess');
+bgColor = function (val, col, nRow) {
+    if (parseFloat(val) > 0) {
+        $("td:eq(" + col + ")", nRow).text("+" + val).addClass('error');
+    } else if (parseFloat(val) <= 0) {
+        $("td:eq(" + col + ")", nRow).addClass('ssuccess');
     }
 }
 getUserId = function () {
@@ -34,16 +34,18 @@ register = function () {
 }
 buy_vip = function () {
     layer.open({
-        type:1,
-        content:$("#vip_frm").html(),
-        area:[
-            '400px','400px'
+        type: 1,
+        content: $("#vip_frm").html(),
+        area: [
+            '400px', '400px'
         ]
     });
 }
+
 function destoryStorage() {
     localStorage.clear();
 }
+
 function saveUserFavor(result) {
     if (result) {
         localStorage['userFavor'] = JSON.stringify(result);
@@ -69,9 +71,19 @@ shutdown = function () {
         window.location = "/";
     });
 }
-layui.use(['element', 'form'], function () {
+layui.use(['element', 'form','util'], function () {
     element = layui.element();
+    var util = layui.util;
     var form = layui.form();
+    util.fixbar({
+        bar1: true,
+        click: function (type) {
+            console.log(type);
+            if (type === 'bar1') {
+                window.open('tencent://message/?uin=790407626');
+            }
+        }
+    });
     form.verify({
         username: function (value) {
             if (!new RegExp("^[a-zA-Z0-9_\u4e00-\u9fa5\\s·]+$").test(value)) {
@@ -178,156 +190,155 @@ function getTime(i) {
 
 
 
-    Vue.http.options.emulateJSON = true;
+Vue.http.options.emulateJSON = true;
 
-    app = new Vue({
-        el: "#app",
-        data: {
-            loginIn: false,
-            userInfo: [],
-            userFavor: [],
-            favorIndex: 0,
-            tabIndex: tabIndex,
-            favorClickIndex: false,
-            favorselectIndex: 0,
-            echarts: null,
-            reflush_url: '',
-            reflushTable:false,
-            time: {
-                current_time: '',
-                last_one: '',
-                last_two: '',
-                last_three: ''
-            }
-        },
-        created: function () {
-            var userID = getUserId();
-            console.log(userID);
-            var self = this;
-            if (localStorage['token']) {
-                Vue.http.headers.common['Authorization'] = localStorage['token'];
-            }
-            if (!userID) {
-                return;
-            }
-            this.$http.get('/user/info').then(function (resp) {
-                self.userInfo = resp.body.result;
+app = new Vue({
+    el: "#app",
+    data: {
+        loginIn: false,
+        userInfo: [],
+        userFavor: [],
+        favorIndex: 0,
+        tabIndex: tabIndex,
+        favorClickIndex: false,
+        favorselectIndex: 0,
+        echarts: null,
+        reflush_url: '',
+        reflushTable: false,
+        time: {
+            current_time: '',
+            last_one: '',
+            last_two: '',
+            last_three: ''
+        }
+    },
+    created: function () {
+        var userID = getUserId();
+        console.log(userID);
+        var self = this;
+        if (localStorage['token']) {
+            Vue.http.headers.common['Authorization'] = localStorage['token'];
+        }
+        if (!userID) {
+            return;
+        }
+        this.$http.get('/user/info').then(function (resp) {
+            self.userInfo = resp.body.result;
 
-                if (self.userInfo.username && self.userInfo.username !== '') {
-                    self.loginIn = true;
-                    if (self.loginIn && localStorage['userFavor']) {
-                        var _userFavor = JSON.parse(localStorage['userFavor']);
-                        if (_userFavor && _userFavor.length > 0) {
-                            self.userFavor = _userFavor;
-                            self.favorIndex = _userFavor[0]['id'];
-                        } else {
-                            app.reflushFavor();
-                        }
+            if (self.userInfo.username && self.userInfo.username !== '') {
+                self.loginIn = true;
+                if (self.loginIn && localStorage['userFavor']) {
+                    var _userFavor = JSON.parse(localStorage['userFavor']);
+                    if (_userFavor && _userFavor.length > 0) {
+                        self.userFavor = _userFavor;
+                        self.favorIndex = _userFavor[0]['id'];
                     } else {
                         app.reflushFavor();
                     }
                 } else {
-                    destoryStorage();
+                    app.reflushFavor();
                 }
-
-            }, function (resp) {
+            } else {
                 destoryStorage();
-                self.loginIn = false;
-            });
-            console.log(self.loginIn);
-            this.reflushTime();
-
-        },
-        updated: function(){
-        },
-        watch: {
-            loginIn: function (newV, oldV) {
-              
-
             }
-        },
-        mounted: function () {
-            
-        },
-        methods: {
-            favorManger: function () {
-                layer.open({
-                    type: 1,
-                    content: $("#favor_manger"),
-                    area: ['320px', '400px']
-                });
-            },
-            rmGroup: function (group_id) {
-                var self = this;
-                layer.confirm('是否删除', function (index) {
-                    self.$http.delete('/user/group/' + group_id)
-                        .then(function (resp) {
-                            layer.msg(resp.body.message);
-                            if (resp.body.status == 200) {
-                                self.reflushFavor();
-                            }
-                        }).catch(function (resp) {
-                            layer.msg("操作失败");
-                        });
-                    layer.close(index);
-                });
-            },
-            reflushTime: function () {
-                this.time.last_one = getTime(1);
-                this.time.last_two = getTime(2);
-                this.time.last_three = getTime(3);
-            },
-            reflushFavor: function () {
-                var self = this;
-                this.$http.get('/user/category').then(function (resp) {
-                    if (resp.body.status == 200) {
 
-                        self.userFavor = JSON.parse(resp.body.result);
-                        saveUserFavor(self.userFavor);
-                    }
-                }).catch(function (fail) {
-                    console.log(JSON.stringify(fail));
-                });
-            },
-            favor_select: function (id) {
-                this.favorIndex = id;
-            },
-            vip_click: function (id) {
-                if (!this.loginIn) {
-                    login();
-                    return false;
-                }
-                this.reflush_url = "/" + id + "/vip";
-            },
-            favor_click: function (id, _public) {
-                var _private = _public || 0
-                var self = this;
-                if (id == 0 && _private == 0) {
-                    self.reflush_url = '';
-                    this.favorClickIndex = false;
-                } else if (_private == 0) {
-                    self.reflush_url = '/' + id + '/0';
-                    this.favorClickIndex = false;
-                } else {
-                    self.reflush_url = '/' + id;
-                    this.favorClickIndex = true;
-                }
-                this.favorselectIndex = id;
-            },
-            save_favor: function (cpy_id) {
-                var self = this;
-                if (self.favorIndex == 0) {
-                    layer.msg('请选择分类');
-                    return false;
-                }
-                this.$http.post('/user/favor/' + cpy_id, {
-                    sg_id: self.favorIndex
-                }).then(function (resp) {
-                    layer.msg(resp.body.message);
-                }).catch(function ($resp) {
-                    layer.msg('网络错误');
-                });
-                return true;
-            }
+        }, function (resp) {
+            destoryStorage();
+            self.loginIn = false;
+        });
+        console.log(self.loginIn);
+        this.reflushTime();
+
+    },
+    updated: function () {},
+    watch: {
+        loginIn: function (newV, oldV) {
+
+
         }
-    });
+    },
+    mounted: function () {
+
+    },
+    methods: {
+        favorManger: function () {
+            layer.open({
+                type: 1,
+                content: $("#favor_manger"),
+                area: ['320px', '400px']
+            });
+        },
+        rmGroup: function (group_id) {
+            var self = this;
+            layer.confirm('是否删除', function (index) {
+                self.$http.delete('/user/group/' + group_id)
+                    .then(function (resp) {
+                        layer.msg(resp.body.message);
+                        if (resp.body.status == 200) {
+                            self.reflushFavor();
+                        }
+                    }).catch(function (resp) {
+                        layer.msg("操作失败");
+                    });
+                layer.close(index);
+            });
+        },
+        reflushTime: function () {
+            this.time.last_one = getTime(1);
+            this.time.last_two = getTime(2);
+            this.time.last_three = getTime(3);
+        },
+        reflushFavor: function () {
+            var self = this;
+            this.$http.get('/user/category').then(function (resp) {
+                if (resp.body.status == 200) {
+
+                    self.userFavor = JSON.parse(resp.body.result);
+                    saveUserFavor(self.userFavor);
+                }
+            }).catch(function (fail) {
+                console.log(JSON.stringify(fail));
+            });
+        },
+        favor_select: function (id) {
+            this.favorIndex = id;
+        },
+        vip_click: function (id) {
+            if (!this.loginIn) {
+                login();
+                return false;
+            }
+            this.reflush_url = "/" + id + "/vip";
+        },
+        favor_click: function (id, _public) {
+            var _private = _public || 0
+            var self = this;
+            if (id == 0 && _private == 0) {
+                self.reflush_url = '';
+                this.favorClickIndex = false;
+            } else if (_private == 0) {
+                self.reflush_url = '/' + id + '/0';
+                this.favorClickIndex = false;
+            } else {
+                self.reflush_url = '/' + id;
+                this.favorClickIndex = true;
+            }
+            this.favorselectIndex = id;
+        },
+        save_favor: function (cpy_id) {
+            var self = this;
+            if (self.favorIndex == 0) {
+                layer.msg('请选择分类');
+                return false;
+            }
+            this.$http.post('/user/favor/' + cpy_id, {
+                sg_id: self.favorIndex
+            }).then(function (resp) {
+                layer.msg(resp.body.message);
+            }).catch(function ($resp) {
+                layer.msg('网络错误');
+            });
+            return true;
+        }
+    }
+});
